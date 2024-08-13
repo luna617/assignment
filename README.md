d711418f0c7b5a61032fa67424567bb88df36bd2
GH6738 <GH6738@surf.com>
2024-08-13 15:03:39 +0300

    assignmnet

---
+++ README.md
@@ -0,0 +1,3 @@
+## Run the code from the terminal using:
+
+uvicorn app_streaming:app --reload
---
+++ user.py
@@ -0,0 +1,17 @@
+import requests
+
+text = open('text.txt', 'r', encoding="utf8").read()
+
+query = {
+    "text": text,
+    "temperature": 2,
+    "max_tokens": 2000,
+    "top_p": 0.2
+}
+url = "http://localhost:8000/summarize"
+
+response = requests.post(url, headers={'Content-Type': 'application/json'}, json=query, stream=True)
+
+for chunk in response.iter_content(chunk_size=10000):
+    if chunk:
+        print(chunk.decode("utf-8"))
