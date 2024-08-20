b89f4a0aaa588b960ca6924a35dcfaddb309ae76
GH6738 <GH6738@surf.com>
2024-08-20 14:32:12 +0300

    fixes

--- app_streaming.py
+++ app_streaming.py
@@ -4,13 +4,15 @@ from transformers import pipeline
 from pydantic import BaseModel
 from fastapi.responses import StreamingResponse
 from typing import Optional
+from asyncer import asyncify
 
 # import uvicorn
+
 
 class GenerationRequest(BaseModel):
     text: str
     temperature: float | None = 0.3
-    max_tokens: Optional[float] = 100
+    max_tokens: Optional[float] = 1000
     top_p: Optional[float] = 0.1
 
 # echo = False
@@ -25,9 +27,18 @@ app = FastAPI()
 
 @app.post("/summarize")
 async def summarize(generation_request: GenerationRequest):
-    translated_text = translate(generation_request.text, src_lang='heb_Hebr', tgt_lang='eng_Latn')
+    translated_text = await asyncify(translate)(generation_request.text, src_lang='heb_Hebr', tgt_lang='eng_Latn')
+    # translated_text = translate(generation_request.text, src_lang='heb_Hebr', tgt_lang='eng_Latn')
     print("Finished translating")
-    generation_request.text = f"Consider the following text: {translated_text}.\nA 5 points summary for the text is: "
+
+    generation_request = {
+        "text": f"Consider the following text: {translated_text}.\nA 5 points summary for the text is: ",
+        "temperature": generation_request.temperature,
+        "max_tokens": generation_request.max_tokens,
+        "top_p": generation_request.top_p
+    }
+
+    generation_request = GenerationRequest(**generation_request)
     return StreamingResponse(five_points_summary(generation_request), media_type='text/event-stream')
 
 
@@ -38,7 +49,7 @@ def translate(text: str, src_lang: str, tgt_lang: str):
 
 async def five_points_summary(generation_request: GenerationRequest):
     print(generation_request)
-    res = generation_request.create_chat_completion(
+    res = instruct_model.create_chat_completion(
         temperature=generation_request.temperature,
         max_tokens=generation_request.max_tokens,
         top_p=generation_request.top_p,
@@ -55,14 +66,17 @@ async def five_points_summary(generation_request: GenerationRequest):
         delta = chunk['choices'][0]['delta']
         if 'content' in delta:
             current_line += delta['content']
-            if delta['content'] == '\n':
-                translated_current_line = translate(current_line, src_lang='eng_Latn', tgt_lang='heb_Hebr')
-                # print(translated_current_line)
+            print(delta['content'])
+            if delta['content'].endswith('\n'):
+                translated_current_line = await asyncify(translate)(current_line, src_lang='eng_Latn', tgt_lang='heb_Hebr')
+                # translated_current_line = translate(current_line, src_lang='eng_Latn', tgt_lang='heb_Hebr')
+                print(translated_current_line)
                 yield translated_current_line
                 current_line = ""
 
-    translated_current_line = await translate(current_line, src_lang='eng_Latn', tgt_lang='heb_Hebr')
-    # print(translated_current_line)
+    translated_current_line = await asyncify(translate)(current_line, src_lang='eng_Latn', tgt_lang='heb_Hebr')
+    # translated_current_line = translate(current_line, src_lang='eng_Latn', tgt_lang='heb_Hebr')
+    print(translated_current_line)
     yield translated_current_line
 
 
